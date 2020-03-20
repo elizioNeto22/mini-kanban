08dbfe6b8d32710853a31c5919ca63a3a5bfd832
Elizio Neto <epe_elizio@hotmail.com>
2020-03-20 01:59:38 -0300

    dropdown 80% fix

--- src/components/column/columnComponent.jsx
+++ src/components/column/columnComponent.jsx
@@ -11,13 +11,29 @@ function Coluna({
   children,
   editar,
   renomear,
+  handleChange,
   addCard,
   deletarColuna,
   // renderCards,
 }) {
   return (
     <div className="column" id={id}>
-      <input type="text" value={title} disabled onClick={editar} />
+      <input type="text" disabled onChange={handleChange} />
+
+      <div onClick={editar} className="options-container">
+        <input type="checkbox" className="options-trigger hidden" id={id} />
+        <label className="options-wrapper" htmlFor={id}>
+          <div className="dots">
+            <div className="options-content">
+              <ul>
+                <li onClick={renomear}>{'Renomear'}</li>
+                <li>{'Excluir'}</li>
+              </ul>
+            </div>
+          </div>
+        </label>
+      </div>
+
       <Dropzone id={id} class_name="dropzone">
         {children}
       </Dropzone>
--- src/components/kanban/kanban_component.jsx
+++ src/components/kanban/kanban_component.jsx
@@ -15,14 +15,15 @@ class Kanban extends React.Component {
         {
           id: uuidv4(),
           title: 'A TESTAR',
-          isEditing: false,
+          isEditing: 'disabled',
+          // isEditing: 'active',
           size: 0,
           cards: [],
         },
         {
           id: uuidv4(),
           title: 'ANDAMENTO',
-          isEditing: false,
+          isEditing: 'disable',
           size: 0,
           cards: [],
         },
@@ -35,9 +36,8 @@ class Kanban extends React.Component {
     this.state.colunas.map((coluna) => {
       const { id } = coluna;
       return (
-        <div>
+        <div key={id}>
           <Coluna
-            key={id}
             {...coluna}
             editar={this.editarColuna}
             renomear={this.renomearColuna}
@@ -51,33 +51,72 @@ class Kanban extends React.Component {
       );
     });
 
-  // Refatorar: handleChange e renomear usar praticamente o mesmo código
-  // FUNÇÕES COLUNA
-  renomearColuna = (e) => {
+  handleChange = (e) => {
     this.setState({ nomeColuna: e.target.value });
-    if (e.key === 'Enter') {
-      const colunaId = e.target.parentNode.id;
-      const novaLista = this.state.colunas.filter((coluna) => {
-        if (coluna.id === colunaId) {
-          coluna.title = e.target.value;
-          coluna.isEditing = false;
-        }
-        return coluna;
-      });
-      this.setState({ colunas: novaLista });
-    }
-  };
-
-  editarColuna = (e) => {
+    // if (e.key === 'Enter') {
     const colunaId = e.target.parentNode.id;
     const novaLista = this.state.colunas.filter((coluna) => {
       if (coluna.id === colunaId) {
-        console.log('coluna id ===:' + colunaId);
-        coluna.isEditing = true;
+        this.setState({});
+        coluna.title = e.target.value;
+        coluna.isEditing = 'disabled';
       }
       return coluna;
     });
-    return this.setState({ colunas: novaLista });
+    this.setState({ colunas: novaLista });
+    // }
+  };
+
+  // Refatorar: handleChange e renomear usar praticamente o mesmo código
+  // FUNÇÕES COLUNA
+  check_open_edit = () => {
+    let edit = 'disabled';
+    this.state.colunas.filter(({ isEditing }) => {
+      // let check = edit
+      if (isEditing === 'active') {
+        window.alert('Please finish editing you column title');
+        // console.log('active_edition');
+        edit = 'active';
+      }
+      return edit;
+    });
+    return edit;
+  };
+
+  editarColuna = (e) => {
+    const openEdition = this.check_open_edit();
+    openEdition === 'active'
+      ? console.log('não pode editar')
+      : console.log(e.target.parentNode.parentNode.childNodes[0].disabled);
+    // const colunaId = e.target.parentNode.id;
+    // const novaLista = this.state.colunas.filter((coluna) => {
+    //   if (coluna.id === colunaId) {
+    //     console.log('coluna id ===:' + colunaId);
+    //     coluna.isEditing = true;
+    //   }
+    //   return coluna;
+    // });
+    // return this.setState({ colunas: novaLista });
+  };
+
+  renomearColuna = (e) => {
+    const element =
+      e.target.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.childNodes[0];
+    // let title = element.disabled;
+    const input = element;
+    element.disabled = false;
+    // this.setState({ nomeColuna: e.target.value });
+    // if (input.key === 'Enter') {
+    //   const colunaId = e.target.parentNode.id;
+    //   const novaLista = this.state.colunas.filter((coluna) => {
+    //     if (coluna.id === colunaId) {
+    //       coluna.title = e.target.value;
+    //       coluna.isEditing = 'disabled';
+    //     }
+    //     return coluna;
+    //   });
+    //   this.setState({ colunas: novaLista });
+    // }
   };
 
   addColuna = (e) => {
